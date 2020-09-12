13336a1aa53432f63392cf6b16079f5361acc0a9
gustavoKMU <gustavo.ruiz.kmu@gmail.com>
2020-09-12 19:37:11 +0900

    added makefile args.c my_args.c

---
+++ args.c
@@ -0,0 +1,16 @@
+/* 
+ * This program prints out information about
+ * its received command-line arguments.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("This program received %d argument(s)\n", argc);
+    
+    for (int i = 0; i < argc; i++) {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
